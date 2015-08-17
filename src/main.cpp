016cb264d3b2cc1c27a27dc53e7a87a011c982b9
Sam Yates <sam.yates@epfl.ch>
2015-08-17 15:36:27 +0200

    Use std::array for 3-d points.
    Addresses heap overhead limiting parallel scalability.
    Also, simplifies openmp constructs in main.

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@
 
 using namespace std;
 
-	/* circulation quantum, core radii, mutual friction */
-	double		kappa = 9.98e-8, a0=1.3e-10, a1=exp(0.5)*a0, alpha=0, q_e=1.6e-19;
+/* circulation quantum, core radii, mutual friction */
+double	kappa = 9.98e-8, a0=1.3e-10, a1=exp(0.5)*a0, alpha=0, q_e=1.6e-19;
 	
 int main(int argc, char* argv[]){
 
@@ -42,60 +42,54 @@ int main(int argc, char* argv[]){
 	cout << "\n\t - - - - - - - -    BEGINNING SIMULATION    - - - - - - - -\n\n";
 	Tangle.mLog << Tangle.StringTime() << "\t\t\t\t\tsimulation begins" << endl;
 
-	#pragma omp parallel
-	{
-		while(i < N_t){
-			#pragma omp master
-			{
-				Tangle.mStep = i;
+	while(i < N_t){
+		Tangle.mStep = i;
 
-				/* save positions to file every mN_f steps */
-				
-				if(i%Tangle.mN_f==0){
-					Tangle.Output(filename, i, file_no);
-					t_temp = clock() -t;
-					printf("\t\t wrote step %6u", i);
-					Tangle.mLog << Tangle.StringTime() << "\t" << setw(10) << Tangle.mStep;
-					Tangle.mLog << "\telapsed: " << ((float)t_temp)/CLOCKS_PER_SEC << " s:\t\twrote to file " << file_no << " for time " << i*us_Dt << " us" << endl;
-					file_no++; 
-				}
-				if(i%100==0 || i%Tangle.mN_f==0){
-					percent = (100*i/N_t);
-					printf("\t\t\r %6.2f %% \t",percent); // output percentage completion
-				}
+		/* save positions to file every mN_f steps */
+		
+		if(i%Tangle.mN_f==0){
+			Tangle.Output(filename, i, file_no);
+			t_temp = clock() -t;
+			printf("\t\t wrote step %6u", i);
+			Tangle.mLog << Tangle.StringTime() << "\t" << setw(10) << Tangle.mStep;
+			Tangle.mLog << "\telapsed: " << ((float)t_temp)/CLOCKS_PER_SEC << " s:\t\twrote to file " << file_no << " for time " << i*us_Dt << " us" << endl;
+			file_no++; 
+		}
+		if(i%100==0 || i%Tangle.mN_f==0){
+			percent = (100*i/N_t);
+			printf("\t\t\r %6.2f %% \t",percent); // output percentage completion
+		}
 
-				/* check for and perform reconnections if required */
-				Tangle.Reconnection();
-				/* adjust mesh until finished */
-				bool MeshFinished(false);	
-				int MeshCount(0);
-				while(MeshFinished==false){
-					MeshFinished = Tangle.MeshAdjust(); MeshCount++; 
-					if(MeshCount == 10){
-						cout << "Mesh too unruly! Exiting..." << endl;
-						Tangle.mLog	<< Tangle.StringTime() << "Mesh too unruly! Program terminated." << endl;
-						exit;
-					}
-				}
-				/* remove rings smaller than 6 points and count them */
-				bool LoopKilled = Tangle.LoopKill(); 
-				if(LoopKilled == true) Tangle.mN_loopkills++;
-			}
-			/* calculate velocities and propagate positions */
-			for(int P=0; P<Tangle.mTangle.size(); P++){
-				#pragma omp for schedule(static) nowait
-				for(int k=0; k<Tangle.mTangle[P]->mN; k++){
-					Point* pField = Tangle.mTangle[P]->mPoints[k];
-					Tangle.CalcVelocityNL(pField);	// calculates non-local contributions to velocity
-				}
-			}
-			#pragma omp master
-			{
-				Tangle.CalcVelocity(); 		// calculates local contributions to velocity
-				Tangle.PropagatePos(Tangle.mDt);	// propagate positions
-				i++;	// step forward
+		/* check for and perform reconnections if required */
+		Tangle.Reconnection();
+		/* adjust mesh until finished */
+		bool MeshFinished(false);	
+		int MeshCount(0);
+		while(MeshFinished==false){
+			MeshFinished = Tangle.MeshAdjust(); MeshCount++; 
+			if(MeshCount == 10){
+				cout << "Mesh too unruly! Exiting..." << endl;
+				Tangle.mLog	<< Tangle.StringTime() << "Mesh too unruly! Program terminated." << endl;
+				exit(1);
 			}
 		}
+		/* remove rings smaller than 6 points and count them */
+		bool LoopKilled = Tangle.LoopKill(); 
+		if(LoopKilled == true) Tangle.mN_loopkills++;
+
+		/* calculate velocities and propagate positions */
+		for(int P=0; P<Tangle.mTangle.size(); P++){
+			int mN=Tangle.mTangle[P]->mN;
+			#pragma omp parallel for
+			for(int k=0; k<mN; k++){
+				Point* pField = Tangle.mTangle[P]->mPoints[k];
+				Tangle.CalcVelocityNL(pField);	// calculates non-local contributions to velocity
+			}
+		}
+
+		Tangle.CalcVelocity(); 		// calculates local contributions to velocity
+		Tangle.PropagatePos(Tangle.mDt);	// propagate positions
+		i++;	// step forward
 	}
 
 	cout << "\n\t - - - - - - -    SIMULATION FINISHED    - - - - - - - -" << endl;
